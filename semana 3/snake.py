8d708284aeb01b2f4a36521d58ea5dd0cadc01b3
RamarangGH <nicolas-fischmann@hotmail.com>
2022-01-10 19:57:55 -0300

    Update snake.py

--- semana 3/snake.py
+++ semana 3/snake.py
@@ -4,10 +4,10 @@ from pygame.locals import *
 def on_grid_random():
     x = random.randint(0,590)
     y = random.randint(0,590)
-    return (x//10 * 10, y//10 * 10)
+    return (x//10*10, y//10*10)
 
-def collision(c1, c2):
-    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+def collision(c1,c2):
+    return ((c1[0]==c2[0]) and (c1[1]==c2[1]))
 
 UP = 0
 RIGHT = 1
@@ -18,24 +18,26 @@ pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake')
 
-snake = [(200, 200), (210, 200), (220,200)]
+snake = [(200,200), (210,200), (220,200)]
 snake_skin = pygame.Surface((10,10))
-snake_skin.fill((255,255,255))
+snake_skin.fill((255,255,255))#branco
 
-apple_pos = on_grid_random()
-apple = pygame.Surface((10,10))
+apple_pos = on_grid_random();
+apple= pygame.Surface((10,10))
 apple.fill((255,0,0))
+
 
 my_direction = LEFT
 
 clock = pygame.time.Clock()
 
-while True:
-    clock.tick(10)
+while True: #laço infinito porque jogo
+    clock.tick(20)
+    
+    #atualização comando
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-
         if event.type == KEYDOWN:
             if event.key == K_UP:
                 my_direction = UP
@@ -45,26 +47,31 @@ while True:
                 my_direction = LEFT
             if event.key == K_RIGHT:
                 my_direction = RIGHT
+        
 
+
+
+    #Atualização posição
+    if my_direction == UP:
+        snake[0] = (snake[0][0], snake[0][1]-10)
+    if my_direction == DOWN:
+        snake[0] = (snake[0][0], snake[0][1]+10)
+    if my_direction == RIGHT:
+        snake[0] = (snake[0][0]+10, snake[0][1])
+    if my_direction == LEFT:
+        snake[0] = (snake[0][0]-10, snake[0][1])
+    
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
         snake.append((0,0))
 
-    for i in range(len(snake) - 1, 0, -1):
+    for i in range(len(snake)-1, 0, -1):
         snake[i] = (snake[i-1][0], snake[i-1][1])
 
-    if my_direction == UP:
-        snake[0] = (snake[0][0], snake[0][1] - 10)
-    if my_direction == DOWN:
-        snake[0] = (snake[0][0], snake[0][1] + 10)
-    if my_direction == RIGHT:
-        snake[0] = (snake[0][0] + 10, snake[0][1])
-    if my_direction == LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])
-
+    #atualização tela
     screen.fill((0,0,0))
-    screen.blit(apple, apple_pos)
-    for pos in snake:
+    screen.blit(apple,apple_pos)
+    for pos in snake:   #movimenta o snake
         screen.blit(snake_skin,pos)
 
     pygame.display.update()
